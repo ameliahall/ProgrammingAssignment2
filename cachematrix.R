21c5692ef6211d302311fac1825200adb6c0cf43
Amelia Weber Hall <ameliahall@utexas.edu>
2015-10-25 16:27:43 -0500

    cleaned up the code, clarified comments and made a small correction in cacheMatrix().

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,19 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a set of two functions that will take an invertible
+## matrix object and see if the inverse has been solved
+## if the inverse has been solved and cached, then the inverted
+## matrix is returned.  If the inverse has not been solved
+## and cached, then cacheSolve will solve and cache the value
+## of the inverted matrix. 
 
 #small dummy matrix for testing:
 #y <- matrix(1:4, 2, 2)
 
 ## Write a short comment describing this function:
 ## This function takes a matrix object and get and sets
-## the value of the matrix and it's inverse
+## the value of the matrix and it's inverse.  Returns
+## a list of values that set and get the inverse of the
+## matrix object x, if it exists.
 
-#9/25: dropped in cache mean....
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -16,7 +21,7 @@ makeCacheMatrix <- function(x = matrix()) {
     m <<- NULL
   }
   get <- function() x
-  setinverse <- function(solve) m <<- solve(x)
+  setinverse <- function(solve) m <<- solve
   getinverse <- function() m
   list(set = set, get = get,
        setinverse = setinverse,
@@ -26,9 +31,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 ## cacheSolve checks to see if the inverse of the matrix is cached
-## and if it is, then solve using the cached value
-## if the inverse is not calculated, calculate the inverse
-## and cache the value for future use
+## and if it is, then returns the cached value.
+## if the inverse is not calculated, cacheSolve will 
+## calculate the inverse and cache the value for future use
 
 cacheSolve <- function(x, ...) {
 ## Return a matrix that is the inverse of 'x'
